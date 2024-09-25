c6f11174252a836191ae7a4e175988ebe58a71a4
Cherry citra <chrryctr1509@gmail.com>
2024-09-25 14:49:27 +0700

    uncommit regis

--- src/Router.js
+++ src/Router.js
@@ -6,15 +6,15 @@ import CircularProgress from '@mui/material/CircularProgress';
 // Lazy load components
 const Home = lazy(() => import("./views/home/HomePage"));
 const Login = lazy(() => import("./views/login/LoginPage"));
-// const Register = lazy(() => import("./views/register/RegisterPage"));
+const Register = lazy(() => import("./views/register/RegisterPage"));
 
 const AppRouter = () => {
   return (
     <Suspense fallback={<CircularProgress />}>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        {/* <Route path="/register" element={<Register />} /> */}
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
         <Route path="*" element={<Home />} />
       </Routes>
     </Suspense>
